556e35265d65cafbb2e468a8f6ca2a111a5d576c
Laurent Hartmann <laurenthartmann.03@gmail.com>
2021-12-10 11:19:12 +0100

    added script for model deployment

--- scripts/train.py
+++ scripts/train.py
@@ -1,61 +1,62 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import LinearRegression
+from sklearn.metrics import f1_score
+from sklearn.ensemble import RandomForestClassifier
 import pickle
-import warnings
-warnings.filterwarnings('ignore')
-
-from feature_engineering import fill_missing_values, drop_column, transform_altitude
-
-# coffee data
-url="https://github.com/jldbc/coffee-quality-database/raw/master/data/robusta_data_cleaned.csv"
-coffee_features=pd.read_csv(url)
-
-# coffee score
-
-url="https://raw.githubusercontent.com/jldbc/coffee-quality-database/master/data/robusta_ratings_raw.csv"
-coffee_quality=pd.read_csv(url)
 
 
-#cleaning data and preparing
-Y = coffee_quality["quality_score"]
-X = coffee_features.select_dtypes(['number'])
+def split_data(df):
+    # define features and target
+    X = df.drop('state', axis=1)
+    y = df['state']
+
+    # split into train and test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3)
+
+    # save splits into dictionary
+    data = {"train": {"X": X_train, "y": y_train},
+            "test": {"X": X_test, "y": y_test}}
+
+    return data
+
+def train_model(data, args):
+    rf_model = RandomForestClassifier(**args)
+    rf_model.fit(data["train"]["X"], data["train"]["y"])
+    return rf_model
+
+def get_model_metrics(model, data):
+    preds = model.predict(data["test"]["X"])
+    f1 = f1_score(data["test"]["y"], preds)
+    metrics = {"f1_score": f1}
+    return metrics
 
 
-# splittin into train and test
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.30, random_state=42)
-## in order to exemplify how the predict will work.. we will save the y_train
-print("Saving test data in the data folder")
-X_test.to_csv("data/X_test.csv", index=False)
-y_test.to_csv("data/y_test.csv", index=False)
+def main():
+    # load data
+    df = pd.read_csv("data/df_preprocessed_log_dummies.csv")
 
-print("Feature engineering on train")
-X_train = transform_altitude(X_train)
-X_train = drop_column(X_train, col_name='Unnamed: 0')
-X_train = drop_column(X_train, col_name='Quakers')
-X_train = fill_missing_values(X_train)
+    # split data into train and test set
+    data = split_data(df)
 
-# model
-print("Training a simple linear regression")
-reg = LinearRegression().fit(X_train, y_train)
-y_train_pred = reg.predict(X_train)
-mse_train = mean_squared_error(y_train, y_train_pred)
+    # train model
+    args = {
+        'n_estimators': 200,
+        'min_samples_split': 200,
+        'min_samples_leaf': 50,
+        'max_features': 'sqrt',
+        'max_depth': 50,
+        'bootstrap': False
+        }
 
-#feature eng on test data
-print("Feature engineering on test")
-X_test = transform_altitude(X_test)
-X_test = drop_column(X_test, col_name='Unnamed: 0')
-X_test = drop_column(X_test, col_name='Quakers')
-X_test = fill_missing_values(X_test)
+    rf = train_model(data, args)
 
-y_test_pred = reg.predict(X_test)
-mse_test = mean_squared_error(y_test, y_test_pred)
+    # get model metrics
+    metrics = get_model_metrics(rf, data)
+    print(metrics)
 
-print (f"MSE on train is: {mse_train}")
-print (f"MSE on test is: {mse_test}")
-print("this is obviously fishy")
-#saving the model
-print("Saving model in the model folder")
-filename = 'models/linear_regression_model.sav'
-pickle.dump(reg, open(filename, 'wb'))
+    # save model
+    model_name = 'models/random_forest_model.sav'
+    pickle.dump(rf, open(model_name, 'wb'))
+
+if __name__ == '__main__':
+    main()
